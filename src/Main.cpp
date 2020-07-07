1773e873d7b4376a394921793f6a44c70ffb29a2
Almas Baimagambetov <almaslvl@gmail.com>
2020-07-07 11:10:14 +0100

    assignment code draft

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,37 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <string>
-// string.h?
 
 #include "SDL.h"
 #include "SDL_net.h"
 
 using namespace std;
 
-// '0' -> 0, '1' -> 1
-int charToInt(char c) {
-    return c - '0';
-}
-
-
-
-
-
 struct GameData {
-    int player1Y;
-    int player2Y;
-    int ballX;
-    int ballY;
+    int player1Y = 0;
+    int player2Y = 0;
+    int ballX = 0;
+    int ballY = 0;
 } game_data;
+
+struct {
+    vector<string> messages;
+} client_data;
 
 const char* IP_NAME = "localhost";
 const Uint16 PORT = 55555;
 
-
-
-
-
-
+bool is_running = true;
 
 static int on_receive(void* socket_ptr) {
     TCPsocket socket = (TCPsocket)socket_ptr;
@@ -47,14 +37,13 @@ static int on_receive(void* socket_ptr) {
 
         char* pch = strtok(message, ",");
 
+        // get the command, which is the first string in the message
         string cmd(pch);
 
-        cout << "Cmd is: " << cmd << endl;
-
+        // then get the arguments to the command
         vector<string> args;
 
         while (pch != NULL) {
-            //printf("%s\n", pch);
             pch = strtok(NULL, ",");
 
             if (pch != NULL) {
@@ -63,8 +52,12 @@ static int on_receive(void* socket_ptr) {
         }
 
         if (cmd == "GAME_DATA") {
-            for (auto arg : args) {
-                cout << arg << endl;
+            // we should have exactly 4 arguments
+            if (args.size() == 4) {
+                game_data.player1Y = stoi(args.at(0));
+                game_data.player2Y = stoi(args.at(1));
+                game_data.ballX = stoi(args.at(2));
+                game_data.ballY = stoi(args.at(3));
             }
         }
 
@@ -72,33 +65,67 @@ static int on_receive(void* socket_ptr) {
             break;
         }
 
-    } while (received > 0);
+    } while (received > 0 && is_running);
+
+    return 0;
+}
+
+static int on_send(void* socket_ptr) {
+    TCPsocket socket = (TCPsocket)socket_ptr;
+
+    while (is_running) {
+        if (client_data.messages.size() > 0) {
+            string message = "CLIENT_DATA";
+
+            for (auto m : client_data.messages) {
+                message += "," + m;
+            }
+
+            client_data.messages.clear();
+
+            SDLNet_TCP_Send(socket, message.c_str(), message.length());
+        }
+
+        SDL_Delay(1);
+    }
 
     return 0;
 }
 
 void loop(SDL_Renderer* renderer) {
-    bool running = true;
-
     SDL_Rect player1 = { 0, 0, 20, 60 };
     SDL_Event event;
 
-    while (running) {
+    while (is_running) {
+        // input
         while (SDL_PollEvent(&event)) {
             if ((event.type == SDL_KEYDOWN || event.type == SDL_KEYUP) && event.key.repeat == 0) {
+                switch (event.key.keysym.sym) {
+                    case SDLK_w:
+                        client_data.messages.push_back(event.type == SDL_KEYDOWN ? "W_DOWN" : "W_UP");
+                        break;
+
+                    case SDLK_ESCAPE:
+                        is_running = false;
+                        break;
+
+                    default:
+                        break;
+                }
             }
 
             if (event.type == SDL_QUIT) {
-                running = false;
+                is_running = false;
             }
         }
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
-        // update_frame
+        // update
         player1.y = game_data.player1Y;
 
+        // render
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
         SDL_RenderDrawRect(renderer, &player1);
 
@@ -130,8 +157,6 @@ int run_game() {
     return 0;
 }
 
-
-
 int main(int argc, char** argv) {
 
     // Initialize SDL
@@ -162,7 +187,8 @@ int main(int argc, char** argv) {
         exit(4);
     }
 
-    SDL_CreateThread(on_receive, "ConnectionThread", (void*)socket);
+    SDL_CreateThread(on_receive, "ConnectionReceiveThread", (void*)socket);
+    SDL_CreateThread(on_send, "ConnectionSendThread", (void*)socket);
 
     run_game();
 
@@ -177,31 +203,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
-
-
-
-
-// send data
-//while (len > 2) {
-//    /* read the buffer from stdin */
-//    printf("Enter Message, or Q to make the server quit:\n");
-//    fgets(message, 1024, stdin);
-//    len = strlen(message);
-
-//    /* strip the newline */
-//    message[len - 1] = '\0';
-
-//    if (len)
-//    {
-//        int result;
-
-//        /* print out the message */
-//        printf("Sending: %.*s\n", len, message);
-
-//        result = SDLNet_TCP_Send(sock, message, len); /* add 1 for the NULL */
-//        if (result < len)
-//            printf("SDLNet_TCP_Send: %s\n", SDLNet_GetError());
-//    }
-
-//}
